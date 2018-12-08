de76a1e62d33e1957225707d9ab5382db3369c36
Simon Gokstorp <simon@gokstorp.se>
2018-12-08 23:20:53 +0000

    Add user interaction

--- python_scripts/question_1.py
+++ python_scripts/question_1.py
@@ -18,74 +18,75 @@ vis_comp = VisMPL.VisCurve2D(vis_config)
 #
 # Initial Heart
 #
+def generate_heart():
+    # Create a B-Spline curve
+    heart = BSpline.Curve()
 
-# Create a B-Spline curve
-heart = BSpline.Curve()
+    # Set up the control points
+    heart.degree = 3
+    heart.ctrlpts = [[0, -20], [25, 5], [17, 20], [5, 19], [0.2, 13], [0, 8], [-0.2, 13], [-5, 19], [-17, 20], [-25, 5], [0, -20]]
 
-# Set up the control points
-heart.degree = 3
-heart.ctrlpts = [[0, -20], [25, 5], [17, 20], [5, 19], [0.2, 13], [0, 8], [-0.2, 13], [-5, 19], [-17, 20], [-25, 5], [0, -20]]
+    # Auto-generate knot vector
+    heart.knotvector = utilities.generate_knot_vector(heart.degree, len(heart.ctrlpts))
 
-# Auto-generate knot vector
-heart.knotvector = utilities.generate_knot_vector(heart.degree, len(heart.ctrlpts))
+    # Return the heart
+    return heart
 
-# Set evaluation delta
-heart.delta = 0.001
+def display_heart(heart, scale=1):
+    # Scale the heart up/down according to the input scale
+    heart.ctrlpts = [[scale*x, scale*y] for x,y in heart.ctrlpts]
 
-# Evaluate curve
-heart.evaluate()
+    # Set evaluation delta
+    heart.delta = 0.001
 
-# Draw the control point polygon and the evaluated curve
-heart.vis = vis_comp
-heart.render()
+    # Evaluate curve
+    heart.evaluate()
 
-#
-# Scaled up Heart
-#
+    # Draw the control point polygon and the evaluated curve
+    heart.vis = vis_comp
+    heart.render()
+    # Downscale the heart again to make sure the control points are not permanently altered
+    heart.ctrlpts = [[x/scale, y/scale] for x,y in heart.ctrlpts]
 
-heart_upscaled = BSpline.Curve()
-scale = 2 # Any 1.0 < scale should work
+if __name__ == "__main__":
+    # Create the heart
+    heart = generate_heart()
+    
+    print("Question 1: generating and displaying a heart at different scales.")
+    # Display the heart at three scales
+    
+    # ORIGINAL size
+    display_heart(heart, 1)
 
-# Set up the control points
-heart_upscaled.degree = 3
-heart_upscaled.ctrlpts = heart.ctrlpts[:]
-# Scale the heart up by multiplying all ctrlpts by *scale*, as the heart is centered around the origin this will also plot at the same place
-heart_upscaled.ctrlpts = [[scale*x, scale*y] for x,y in heart_upscaled.ctrlpts]
+    # scaled UP
+    display_heart(heart, 2)
 
-# Auto-generate knot vector
-heart_upscaled.knotvector = utilities.generate_knot_vector(heart_upscaled.degree, len(heart_upscaled.ctrlpts))
+    # scaled DOWN
+    display_heart(heart, 0.5)
+    while True:
+        UserInput = input("To display again with the default values (scale = [1, 2, 0.5]), type: 'default'.\n\
+To display with your own scale, simply type 'scale', then at the next prompt type in your desired value.\n\
+To quit this program, type 'quit'\n\
+Input: ").lower()
 
-# Set evaluation delta
-heart_upscaled.delta = 0.001
+        if UserInput == 'quit' or UserInput == 'q' or UserInput == 'exit':
+            print('Quitting Question 4 program.')
+            break
+        elif UserInput == 'default':
+            # Display the heart at three scales
 
-# Evaluate curve
-heart_upscaled.evaluate()
+            # LOW quality
+            display_heart(heart, 1)
 
-# Draw the control point polygon and the evaluated curve
-heart_upscaled.vis = vis_comp
-heart_upscaled.render()
+            # MEDIUM quality
+            display_heart(heart, 2)
 
-#
-# Scaled down Heart
-#
-
-heart_downscaled = BSpline.Curve()
-scale = 0.5 # Any 0 < scale < 1.0 should work (barring floating point inaccuracies) for downscaling
-
-# Set up the control points
-heart_downscaled.degree = 3
-heart_downscaled.ctrlpts = [[0, -20], [25, 5], [17, 20], [5, 19], [0.2, 13], [0, 8], [-0.2, 13], [-5, 19], [-17, 20], [-25, 5], [0, -20]]
-heart_downscaled.ctrlpts = [[scale*x, scale*y] for x,y in heart_downscaled.ctrlpts]
-
-# Auto-generate knot vector
-heart_downscaled.knotvector = utilities.generate_knot_vector(heart_downscaled.degree, len(heart_downscaled.ctrlpts))
-
-# Set evaluation delta
-heart_downscaled.delta = 0.001
-
-# Evaluate curve
-heart_downscaled.evaluate()
-
-# Draw the control point polygon and the evaluated curve
-heart_downscaled.vis = vis_comp
-heart_downscaled.render()
+            # HIGH quality
+            display_heart(heart, 0.5)
+        elif UserInput == 'scale':
+            try:
+                scale = float(input("Scale value: "))
+            except ValueError:
+                print(e)
+                continue
+            display_heart(heart, scale)
