1a0b574fec6a42d33b6dfe07d8b9df8bf941865e
Simon Gokstorp <simon@gokstorp.se>
2018-12-08 22:51:26 +0000

    Add user interaction to question 4

--- python_scripts/question_4.py
+++ python_scripts/question_4.py
@@ -11,9 +11,9 @@ from question_3 import *
 if __name__ == "__main__":
     # Generate the landscape
     landscape = generate_landscape()
-
-    # Display the landscape at 3 levels of detail
-
+    print("Question 4: generating the landscape from Question 3 with varying levels of detail.")
+    # Plot it at the 3 levels of detail first
+    
     # LOW quality
     display_landscape(landscape, 0.02)
 
@@ -22,3 +22,37 @@ if __name__ == "__main__":
 
     # HIGH quality
     display_landscape(landscape, 0.08)
+    while True:
+        UserInput = input("To plot again with the default values (delta = [0.02, 0.04, 0.08]), type: 'default'.\n\
+To plot with your own values, simply type 'delta' or 'sample_size', then at the next prompt type in your desired value.\n\
+To quit this program, type 'quit'\n\
+Input: ").lower()
+
+        if UserInput == 'quit' or UserInput == 'q' or UserInput == 'exit':
+            print('Quitting Question 4 program.')
+            break
+        elif UserInput == 'default':
+            # Display the landscape at 3 levels of detail
+
+            # LOW quality
+            display_landscape(landscape, 0.02)
+
+            # MEDIUM quality
+            display_landscape(landscape, 0.04)
+
+            # HIGH quality
+            display_landscape(landscape, 0.08)
+        elif UserInput == 'delta':
+            try:
+                delta = float(input("Delta value: "))
+            except ValueError:
+                print(e)
+                continue
+            print("test")
+            display_landscape(landscape, delta)
+        elif UserInput == 'sample_size':
+            try:
+                delta = 1/float(input("Sample size: "))
+            except ValueError:
+                continue
+            display_landscape(landscape, delta)
