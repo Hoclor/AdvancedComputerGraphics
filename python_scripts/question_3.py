955b41147546fc1a4740be2c3c6125515cb02622
Simon Gokstorp <simon@gokstorp.se>
2018-12-08 22:28:31 +0000

    Auto display landscape if file is main

--- python_scripts/question_3.py
+++ python_scripts/question_3.py
@@ -32,111 +32,111 @@ def set_x_y(region, x_offset, y_offset):
 #
 
 def generate_landscape():
-# Create a B-Spline surface instance
-landscape = BSpline.Surface()
+    # Create a B-Spline surface instance
+    landscape = BSpline.Surface()
 
-# Set up the Bezier surface
-landscape.degree_u = 3
-landscape.degree_v = 3
-landscape.ctrlpts_size_u = 17 # x axis (visually vertical)
-landscape.ctrlpts_size_v = 14 # y axis (visually horizontal)
+    # Set up the Bezier surface
+    landscape.degree_u = 3
+    landscape.degree_v = 3
+    landscape.ctrlpts_size_u = 17 # x axis (visually vertical)
+    landscape.ctrlpts_size_v = 14 # y axis (visually horizontal)
 
-# Use numpy arrays to define each section of the landscape to allow for easier piecewise and global processing
+    # Use numpy arrays to define each section of the landscape to allow for easier piecewise and global processing
 
-# Define the 7x7 mountain section
-mountain = np.array([[0, 0, 10], [0, 0, 8], [0, 0, 8], [0, 0, 10], [0, 0, 10], [0, 0, 25], [0, 0, 10],
-                     [0, 0, 12], [0, 0, 24], [0, 0, 48], [0, 0, 24], [0, 0, 44], [0, 0, 65], [0, 0, 20],
+    # Define the 7x7 mountain section
+    mountain = np.array([[0, 0, 10], [0, 0, 8], [0, 0, 8], [0, 0, 10], [0, 0, 10], [0, 0, 25], [0, 0, 10],
+                        [0, 0, 12], [0, 0, 24], [0, 0, 48], [0, 0, 24], [0, 0, 44], [0, 0, 65], [0, 0, 20],
                         [0, 0, 10], [0, 0, 29], [0, 0, 75], [0, 0, 40], [0, 0, 28], [0, 0, 33], [0, 0, 24],
-                     [0, 0, 10], [0, 0, 15], [0, 0, 55], [0, 0, 31], [0, 0, 15], [0, 0, 13], [0, 0, 8],
+                        [0, 0, 10], [0, 0, 15], [0, 0, 55], [0, 0, 31], [0, 0, 15], [0, 0, 13], [0, 0, 8],
                         [0, 0, -3], [0, 0, 0], [0, 0, 25], [0, 0, 48], [0, 0, 38], [0, 0, 40], [0, 0, 14],
                         [0, 0, -3], [0, 0, -4], [0, 0, -3], [0, 0, 23], [0, 0, 70], [0, 0, 85], [0, 0, 30],
                         [0, 0, 5], [0, 0, -8], [0, 0, -4], [0, 0, 15], [0, 0, 40], [0, 0, 35], [0, 0, 20]]).reshape(7,7,3)
-# Set its global position - top left
-mountain = set_x_y(mountain, 0, 0)
+    # Set its global position - top left
+    mountain = set_x_y(mountain, 0, 0)
 
-# Define the 7x7 bumpy plains section
-bumpy_plains = np.array([[0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10],
+    # Define the 7x7 bumpy plains section
+    bumpy_plains = np.array([[0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10],
                             [0, 0, 10], [0, 0, 10], [0, 0, 20], [0, 0, 20], [0, 0, 40], [0, 0, 30], [0, 0, 10],
                             [0, 0, 20], [0, 0, 30], [0, 0, 20], [0, 0, 60], [0, 0, 80], [0, 0, 10], [0, 0, 10],
                             [0, 0, 15], [0, 0, 20], [0, 0, 40], [0, 0, 20], [0, 0, 70], [0, 0, 20], [0, 0, 10],
                             [0, 0, 40], [0, 0, 10], [0, 0, 30], [0, 0, 60], [0, 0, 70], [0, 0, 20], [0, 0, 10],
                             [0, 0, 40], [0, 0, 40], [0, 0, 40], [0, 0, 20], [0, 0, 30], [0, 0, 40], [0, 0, 10],
                             [0, 0, 20], [0, 0, 40], [0, 0, 20], [0, 0, 10], [0, 0, 40], [0, 0, 40], [0, 0, 10]]).reshape(7,7,3)
-# Set its global position - top right
-bumpy_plains = set_x_y(bumpy_plains, 0, 35)
+    # Set its global position - top right
+    bumpy_plains = set_x_y(bumpy_plains, 0, 35)
 
 
 
-        
 
+            
 
-# Define the 10x7 hill section
+    # Define the 10x7 hill section
     hill = np.array([[0, 0, 10], [0, 0, -8], [0, 0, 0], [0, 0, 8], [0, 0, 10], [0, 0, 10], [0, 0, 10],
                     [0, 0, 10], [0, 0, -10], [0, 0, -10], [0, 0, 5], [0, 0, -10], [0, 0, -10], [0, 0, -10],
                     [0, 0, 5], [0, 0, 5], [0, 0, 0], [0, 0, -10], [0, 0, -10], [0, 0, -5], [0, 0, 5],
-                 [0, 0, 5], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10],
-                 [0, 0, 5], [0, 0, 13], [0, 0, 18], [0, 0, 20], [0, 0, 14], [0, 0, 11], [0, 0, 5],
-                 [0, 0, 5], [0, 0, 18], [0, 0, 22], [0, 0, 25], [0, 0, 17], [0, 0, 10], [0, 0, 5],
-                 [0, 0, 5], [0, 0, 24], [0, 0, 25], [0, 0, 28], [0, 0, 25], [0, 0, 13], [0, 0, 5],
-                 [0, 0, 5], [0, 0, 14], [0, 0, 22], [0, 0, 25], [0, 0, 28], [0, 0, 9], [0, 0, 5],
-                 [0, 0, 5], [0, 0, 9], [0, 0, 18], [0, 0, 20], [0, 0, 11], [0, 0, 6], [0, 0, 5],
-                 [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5]]).reshape(10,7,3)
-# Set its global position - bottom left
-hill = set_x_y(hill, 35, 0)
+                    [0, 0, 5], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10], [0, 0, 10],
+                    [0, 0, 5], [0, 0, 13], [0, 0, 18], [0, 0, 20], [0, 0, 14], [0, 0, 11], [0, 0, 5],
+                    [0, 0, 5], [0, 0, 18], [0, 0, 22], [0, 0, 25], [0, 0, 17], [0, 0, 10], [0, 0, 5],
+                    [0, 0, 5], [0, 0, 24], [0, 0, 25], [0, 0, 28], [0, 0, 25], [0, 0, 13], [0, 0, 5],
+                    [0, 0, 5], [0, 0, 14], [0, 0, 22], [0, 0, 25], [0, 0, 28], [0, 0, 9], [0, 0, 5],
+                    [0, 0, 5], [0, 0, 9], [0, 0, 18], [0, 0, 20], [0, 0, 11], [0, 0, 6], [0, 0, 5],
+                    [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5]]).reshape(10,7,3)
+    # Set its global position - bottom left
+    hill = set_x_y(hill, 35, 0)
 
-# Define the 10x7 lake section
-lake = np.array([[0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5],
-                  [0, 0, -15], [0, 0, -13], [0, 0, -13], [0, 0, 0], [0, 0, 3], [0, 0, 5], [0, 0, 5],
-                  [0, 0, 5], [0, 0, -13], [0, 0, -15], [0, 0, -13], [0, 0, 0], [0, 0, 3], [0, 0, 5],
-                  [0, 0, 5], [0, 0, -13], [0, 0, -18], [0, 0, -15], [0, 0, -13], [0, 0, 0], [0, 0, 5],
-                  [0, 0, 5], [0, 0, -13], [0, 0, -15], [0, 0, -18], [0, 0, -15], [0, 0, -13], [0, 0, 5],
-                  [0, 0, 5], [0, 0, -10], [0, 0, -15], [0, 0, -18], [0, 0, -20], [0, 0, -15], [0, 0, 5],
-                  [0, 0, 5], [0, 0, 3], [0, 0, 0], [0, 0, -13], [0, 0, -20], [0, 0, -13], [0, 0, 5],
-                  [0, 0, 5], [0, 0, 3], [0, 0, 0], [0, 0, -13], [0, 0, -5], [0, 0, -13], [0, 0, 5],
+    # Define the 10x7 lake section
+    lake = np.array([[0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5],
+                    [0, 0, -15], [0, 0, -13], [0, 0, -13], [0, 0, 0], [0, 0, 3], [0, 0, 5], [0, 0, 5],
+                    [0, 0, 5], [0, 0, -13], [0, 0, -15], [0, 0, -13], [0, 0, 0], [0, 0, 3], [0, 0, 5],
+                    [0, 0, 5], [0, 0, -13], [0, 0, -18], [0, 0, -15], [0, 0, -13], [0, 0, 0], [0, 0, 5],
+                    [0, 0, 5], [0, 0, -13], [0, 0, -15], [0, 0, -18], [0, 0, -15], [0, 0, -13], [0, 0, 5],
+                    [0, 0, 5], [0, 0, -10], [0, 0, -15], [0, 0, -18], [0, 0, -20], [0, 0, -15], [0, 0, 5],
+                    [0, 0, 5], [0, 0, 3], [0, 0, 0], [0, 0, -13], [0, 0, -20], [0, 0, -13], [0, 0, 5],
+                    [0, 0, 5], [0, 0, 3], [0, 0, 0], [0, 0, -13], [0, 0, -5], [0, 0, -13], [0, 0, 5],
                     [0, 0, 5], [0, 0, 3], [0, 0, 2], [0, 0, 3], [0, 0, 5], [0, 0, 6], [0, 0, 5],
-                  [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5]]).reshape(10,7,3)
-# Set its global position - bottom right
-lake = set_x_y(lake, 35, 35)
+                    [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5], [0, 0, 5]]).reshape(10,7,3)
+    # Set its global position - bottom right
+    lake = set_x_y(lake, 35, 35)
 
-# Resulting layout:
-#  -> v axis
-#  mountains |   bumpy_plains
-# ------------------
-#   hill   |   lake
+    # Resulting layout:
+    #  -> v axis
+    #  mountains |   bumpy_plains
+    # ------------------
+    #   hill   |   lake
 
-# Join the mountains and bumpy_plains up along the v axis (horizontally)
-global_pts = np.hstack((mountain, bumpy_plains))
-# Do the same for hill and lake
-temp_pts = np.hstack((hill, lake))
-# Join these two up together vertically to create the final layout, outlined above
-global_pts = np.vstack((global_pts, temp_pts))
-# reshape global_pts to be a flat list
-global_pts = global_pts.reshape(-1, 3)
+    # Join the mountains and bumpy_plains up along the v axis (horizontally)
+    global_pts = np.hstack((mountain, bumpy_plains))
+    # Do the same for hill and lake
+    temp_pts = np.hstack((hill, lake))
+    # Join these two up together vertically to create the final layout, outlined above
+    global_pts = np.vstack((global_pts, temp_pts))
+    # reshape global_pts to be a flat list
+    global_pts = global_pts.reshape(-1, 3)
 
-# convert global_pts from numpy array to python list
-global_pts = global_pts.tolist()
-landscape.ctrlpts = global_pts
+    # convert global_pts from numpy array to python list
+    global_pts = global_pts.tolist()
+    landscape.ctrlpts = global_pts
 
     # Return the landscape
     return landscape
 
 def display_landscape(landscape, delta=0.04):
-# Auto-generate knot vector
-landscape.knotvector_u = utilities.generate_knot_vector(landscape.degree_u, landscape.ctrlpts_size_u)
-landscape.knotvector_v = utilities.generate_knot_vector(landscape.degree_v, landscape.ctrlpts_size_v)
+    # Auto-generate knot vector
+    landscape.knotvector_u = utilities.generate_knot_vector(landscape.degree_u, landscape.ctrlpts_size_u)
+    landscape.knotvector_v = utilities.generate_knot_vector(landscape.degree_v, landscape.ctrlpts_size_v)
 
-# Set evaluation delta
-landscape.delta = 0.04
+    # Set evaluation delta
+    landscape.delta = 0.04
 
-# Evaluate curve
-landscape.evaluate()
+    # Evaluate curve
+    landscape.evaluate()
 
-# Draw the control point polygon and the evaluated curve
-# Prepare the VisConfig
-vis_config = VisMPL.VisConfig(ctrlpts=False)
+    # Draw the control point polygon and the evaluated curve
+    # Prepare the VisConfig
+    vis_config = VisMPL.VisConfig(ctrlpts=False)
 
-vis_comp = VisMPL.VisSurfTriangle(vis_config)
-landscape.vis = vis_comp
+    vis_comp = VisMPL.VisSurfTriangle(vis_config)
+    landscape.vis = vis_comp
     landscape.render(colormap=cm.get_cmap(name='terrain')) # Apply a colormap to the render
 
 #
@@ -162,3 +162,11 @@ def generate_mountains(x_size, y_size):
             mountain[x, y] = [0, 0, z]
     # return the mountain array
     return mountain
+
+
+if __name__ == "__main__":
+    # Generate the landscape
+    landscape = generate_landscape()
+
+    # Display the landscape
+    display_landscape(landscape, 0.04)
